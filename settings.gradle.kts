1da73132136076b3be80acf00b927adb2752bf97
jukov <a@jukov.info>
2026-08-10 23:30:34 +0700

    Fix R8 Kotlin metadata compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,15 @@
 rootProject.name = "Jukovplayer"
 
 pluginManagement {
+    buildscript {
+        repositories {
+            maven("https://storage.googleapis.com/r8-releases/raw")
+        }
+        dependencies {
+            // AGP 9.0.1 bundles an R8 version that cannot parse Kotlin 2.4 metadata.
+            classpath("com.android.tools:r8:9.1.29")
+        }
+    }
     repositories {
         google {
             mavenContent {
